081e3ea214ed446f6e5fadab8d73f2e228aaf2b6
wb_maqianbo <wb_maqianbo@meituan.com>
2023-03-22 22:49:42 +0800

    feat(Other): 更改eslint规则,增加commit提交规范

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,13 @@ module.exports = {
     root: true,
     env: {
         node: true,
-        browser: true,
-        es6: true,
     },
     extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
     parserOptions: {
-        parser: 'babel-eslint',
+        ecmaVersion: 2020,
     },
+
+    plugins: ['vue'],
     rules: {
         'no-multiple-empty-lines': 0, // 禁用不允许多行空格
     },
